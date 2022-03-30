39b19503d75acad78a43efb0847912167bff62ac
rohit kumar <kumarrohit6324@gmail.com>
2022-03-30 20:32:28 +0530

    improvised and made the cart look better if empty

--- script/common.js
+++ script/common.js
@@ -1,3 +1,7 @@
 document.querySelector("#user-cart").addEventListener("click",function(){
     window.location.href = "../cart.html";
 })
+
+document.querySelector("header > svg").addEventListener("click",function(){
+    window.location.href = "../index.html";
+})

003e5fa0563df4b8e11e0bab705c7de348ce2cc8
rohit kumar <kumarrohit6324@gmail.com>
2022-03-31 02:21:58 +0530

    first step of final assembling

--- script/common.js
+++ script/common.js
@@ -5,3 +5,40 @@ document.querySelector("#user-cart").addEventListener("click",function(){
 document.querySelector("header > svg").addEventListener("click",function(){
     window.location.href = "../index.html";
 })
+
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(1)").addEventListener("click",function(){
+    window.location.href = "../furniture.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(2)").addEventListener("click",function(){
+    window.location.href = "../rugs.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(3)").addEventListener("click",function(){
+    window.location.href = "../decor.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(4)").addEventListener("click",function(){
+    window.location.href = "../bed.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(5)").addEventListener("click",function(){
+    window.location.href = "../homeimprov.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(6)").addEventListener("click",function(){
+    window.location.href = "../kitchen.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(7)").addEventListener("click",function(){
+    window.location.href = "../outdoor.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(8)").addEventListener("click",function(){
+    window.location.href = "../jwelry.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(9)").addEventListener("click",function(){
+    window.location.href = "../lighting.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(10)").addEventListener("click",function(){
+    window.location.href = "../kids.html";
+})
+document.querySelector("nav > ul:nth-child(1) > li:nth-child(11)").addEventListener("click",function(){
+    window.location.href = "../more.html";
+})
+// document.querySelector("nav > ul > .crimson").addEventListener("click",function(){
+//     window.location.href = "../restagcards.html";
+// })

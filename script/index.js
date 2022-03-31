e150c11968688854d29f59e1a992772016a91b41
rohit kumar <kumarrohit6324@gmail.com>
2022-03-31 23:51:24 +0530

    added links to index

---
+++ script/index.js
@@ -0,0 +1,18 @@
+document.querySelector("#limitedCards > div:nth-child(1)").addEventListener("click",function(){
+    window.location.href = "../rugscards.html";
+})
+document.querySelector("#limitedCards > div:nth-child(2)").addEventListener("click",function(){
+    window.location.href = "../gardencards.html";
+})
+document.querySelector("#limitedCards > div:nth-child(3)").addEventListener("click",function(){
+    window.location.href = "../furniturecards.html";
+})
+document.querySelector("#limitedCards > div:nth-child(4)").addEventListener("click",function(){
+    window.location.href = "../lightingcards.html";
+})
+document.querySelector("#limitedCards > div:nth-child(5)").addEventListener("click",function(){
+    window.location.href = "../beddingcards.html";
+})
+document.querySelector("#limitedCards > div:nth-child(6)").addEventListener("click",function(){
+    window.location.href = "../restagcards.html";
+})
